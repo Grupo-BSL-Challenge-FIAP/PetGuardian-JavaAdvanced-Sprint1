03f8d9b92aed615a57976abb6d3f4fd6e769bd1b
manuelalacerda <manuelalacerda88@gmail.com>
2026-08-24 23:48:55 -0300

    feat: implementa camada de visualizaçao frontend

--- src/main/java/br/com/clyvo/petguardian/config/SecurityConfig.java
+++ src/main/java/br/com/clyvo/petguardian/config/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         // Rotas públicas
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers("/").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register/vet").permitAll()
                         .requestMatchers("/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
