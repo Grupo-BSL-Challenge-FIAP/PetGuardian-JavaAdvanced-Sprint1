bc2ba734f5b9b117ec05fa916f03d0339e6e0739
manuelalacerda <manuelalacerda88@gmail.com>
2026-08-31 10:54:24 -0300

    refactor(repository): alinha métodos de busca com as remoções e renomeações da entidade Pet

--- src/main/java/br/com/clyvo/vitalia/repository/PetRepository.java
+++ src/main/java/br/com/clyvo/vitalia/repository/PetRepository.java
@@ -1,7 +1,6 @@
 package br.com.clyvo.vitalia.repository;
 
 import br.com.clyvo.vitalia.entity.Pet;
-import br.com.clyvo.vitalia.enums.CurrentStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,8 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PetRepository extends JpaRepository<Pet, Long> {
     Page<Pet> findByNameContainingIgnoreCase(String name, Pageable pageable);
-    Page<Pet> findBySpeciesIgnoreCase(String species, Pageable pageable);
-    Page<Pet> findByCurrentStatus(CurrentStatus currentStatus, Pageable pageable);
-
-    Page<Pet> findByResponsibleId(Long responsibleId, Pageable pageable);
+    Page<Pet> findByOwnerUserId(Long ownerUserId, Pageable pageable);
 }
