6b032b855b2aedb9718602f9e19b184c11a3a050
manuelalacerda <manuelalacerda88@gmail.com>
2026-08-31 18:15:40 -0300

    feat: atualiza SecurityConfig para injetar dependencias com Lombok e ajustar rotas

--- src/main/java/br/com/clyvo/vitalia/security/SecurityConfig.java
+++ src/main/java/br/com/clyvo/vitalia/security/SecurityConfig.java
@@ -1,6 +1,6 @@
 package br.com.clyvo.vitalia.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -16,10 +16,10 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Autowired
-    private SecurityFilter securityFilter;
+    private final SecurityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -49,7 +49,6 @@ public class SecurityConfig {
                         // Qualquer outra requisição precisa apenas estar autenticada
                         .anyRequest().authenticated()
                 )
-                .headers(headers -> headers.frameOptions(frame -> frame.disable())) // Permite o H2 Console
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
