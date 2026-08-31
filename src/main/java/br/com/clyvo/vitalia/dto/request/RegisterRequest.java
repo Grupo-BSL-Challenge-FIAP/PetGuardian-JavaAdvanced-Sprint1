c533f3a97ebb474865d7a64bd2210ec194885c5d
manuelalacerda <manuelalacerda88@gmail.com>
2026-08-31 10:00:56 -0300

    refactor(dto): adiciona campo fullName e validações no RegisterRequest

--- src/main/java/br/com/clyvo/vitalia/dto/request/RegisterRequest.java
+++ src/main/java/br/com/clyvo/vitalia/dto/request/RegisterRequest.java
@@ -1,7 +1,19 @@
 package br.com.clyvo.vitalia.dto.request;
 
 import br.com.clyvo.vitalia.enums.Role;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record RegisterRequest (String email, String password, Role role){
-    
-}
+public record RegisterRequest(
+        @NotBlank(message = "O nome completo é obrigatório")
+        String fullName,
+
+        @NotBlank(message = "O e-mail é obrigatório")
+        String email,
+
+        @NotBlank(message = "A senha é obrigatória")
+        String password,
+
+        @NotNull(message = "O papel (role) é obrigatório")
+        Role role
+) {}
